e287212d820df8e396abec76eb5483e48880f471
Juan Carlo <46091996+jcgonzalezasj@users.noreply.github.com>
2022-07-21 20:50:16 -0300

    feat: creating initial form and components

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Lista() {
+    return(
+        <aside>
+            <h2>Matérias do dia</h2>
+            <ul>
+                <li>
+                    <h3>React</h3>
+                    <span>01:00:00</span>
+                </li>
+                <li>
+                    <h3>JS</h3>
+                    <span>01:00:00</span>
+                </li>
+            </ul>
+        </aside>
+    )
+} 
+
+export default Lista;

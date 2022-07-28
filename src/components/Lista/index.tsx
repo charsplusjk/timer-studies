8224e9528284a37389a0e6bdcd026329e3279da4
jcgonzalezasj <46091996+jcgonzalezasj@users.noreply.github.com>
2022-07-28 20:39:09 -0300

    Lists

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 
 function Lista() {
-    return(
+    const tarefas = [{
+		tarefa: "React",
+		tempo: "02:00:00"
+	}, {
+		tarefa: "js",
+		tempo: "01:00:00"
+		}]
+
+	return(
         <aside>
             <h2>Matérias do dia</h2>
             <ul>
-                <li>
-                    <h3>React</h3>
-                    <span>01:00:00</span>
-                </li>
-                <li>
-                    <h3>JS</h3>
-                    <span>01:00:00</span>
-                </li>
-            </ul>
+				{tarefas.map((item, index) => (
+						<li key="{index}">
+								<h3>{item.tarefa}</h3>
+								<span>{item.tempo}</span>
+						</li>
+				))}
+		    </ul>
         </aside>
     )
 } 

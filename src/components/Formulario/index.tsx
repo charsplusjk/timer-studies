f896b24eed01d0252ae76ea7662c9c1170f5a9a8
Juan Carlo <46091996+jcgonzalezasj@users.noreply.github.com>
2022-07-29 13:01:08 -0300

    fix: DRY - button

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -33,7 +33,7 @@ class Formulario extends React.Component {
                         required
                     />
                 </div>
-                <Botao />
+                <Botao texto="Adicionar" />
             </form>
         )
     }

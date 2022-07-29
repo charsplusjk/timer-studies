84c256411301e54b5dd6a5353deb57d2bd91c2ea
jcgonzalezasj <46091996+jcgonzalezasj@users.noreply.github.com>
2022-07-29 18:57:29 -0300

    Refactoring the list

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import style from './Lista.module.scss';
+import Item from './Item';
 
 function Lista() {
     const tarefas = [{
@@ -15,10 +16,10 @@ function Lista() {
             <h2>Matérias do dia</h2>
             <ul>
 				{tarefas.map((item, index) => (
-						<li className={style.item} key="{index}">
-								<h3>{item.tarefa}</h3>
-								<span>{item.tempo}</span>
-						</li>
+					<Item
+					key="{index}"
+					{...item}
+					/>			
 				))}
 		    </ul>
         </aside>

e7f2aa4e25039a013235315b7d83bd98244f3c6a
jcgonzalezasj <46091996+jcgonzalezasj@users.noreply.github.com>
2022-07-30 12:08:40 -0300

    state

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useState } from 'react';
 import style from './Lista.module.scss';
 import Item from './Item';
 
 function Lista() {
-    const tarefas = [{
-		tarefa: "React",
-		tempo: "02:00:00"
+	const [tarefas, setTarefas] = useState([{
+		tarefa: 'React',
+		tempo: '02:00:00'
 	}, {
-		tarefa: "js",
-		tempo: "01:00:00"
-		}]
-
+		tarefa: 'JS',
+		tempo: '01:00:00'
+	}]);
 	return(
         <aside className={style.listaTarefas}>
-            <h2>Matérias do dia</h2>
+            <h2 onClick={() => {
+	    		setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}])
+			}}>Matérias do dia</h2>
             <ul>
 				{tarefas.map((item, index) => (
 					<Item
